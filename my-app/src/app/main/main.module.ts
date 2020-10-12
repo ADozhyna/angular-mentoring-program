e9ed9c7f62fa05c4f59050e0dc9fde2505d66c79
Hanna Dozhyna <Hanna_Dozhyna@epam.com>
2020-10-12 13:09:07 +0300

    feat: search-control component

--- my-app/src/app/main/main.module.ts
+++ my-app/src/app/main/main.module.ts
@@ -2,16 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CoursesPageComponent } from './components/courses-page/courses-page.component';
 import { MainRoutingModule } from './main-routing.module';
+import { SearchControlComponent } from './components/search-control/search-control.component';
+import { SharedModule } from '../shared/shared.module';
+
 
 
 
 @NgModule({
   declarations: [
-    CoursesPageComponent
+    CoursesPageComponent,
+    SearchControlComponent,
   ],
   imports: [
     CommonModule,
-    MainRoutingModule
+    MainRoutingModule,
+    SharedModule,
   ],
   exports: [
     CoursesPageComponent
--- my-app/src/app/shared/shared.module.ts
+++ my-app/src/app/shared/shared.module.ts
@@ -1,17 +1,29 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FlexLayoutModule } from '@angular/flex-layout';
-
+import { MatFormFieldModule} from '@angular/material/form-field';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatInputModule,
+    FormsModule
+
   ],
   exports: [
-    FlexLayoutModule
+    FlexLayoutModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatInputModule,
+    FormsModule
   ]
 })
 export class SharedModule { }
