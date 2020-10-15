9fdac170f0dd6b6e72eef10cdb3e849b90efd09c
Hanna Dozhyna <Hanna_Dozhyna@epam.com>
2020-10-15 13:34:42 +0300

    fix: bugs from task-2

--- my-app/src/app/core/components/breadcrumbs/breadcrumbs.component.spec.ts
+++ my-app/src/app/core/components/breadcrumbs/breadcrumbs.component.spec.ts
@@ -1,20 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { BreadcrumpsComponent } from './breadcrumps.component';
+import { BreadcrumbsComponent } from './breadcrumbs.component';
 
-describe('BreadcrumpsComponent', () => {
-  let component: BreadcrumpsComponent;
-  let fixture: ComponentFixture<BreadcrumpsComponent>;
+describe('BreadcrumbsComponent', () => {
+  let component: BreadcrumbsComponent;
+  let fixture: ComponentFixture<BreadcrumbsComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ BreadcrumpsComponent ]
+      declarations: [ BreadcrumbsComponent ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(BreadcrumpsComponent);
+    fixture = TestBed.createComponent(BreadcrumbsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
