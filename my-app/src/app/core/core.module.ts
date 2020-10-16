47ab2809e58612b72ec0e7f20ce4bfc317039ee0
Hanna Dozhyna <Hanna_Dozhyna@epam.com>
2020-10-16 11:08:51 +0300

    feat: courses service

--- my-app/src/app/core/core.module.ts
+++ my-app/src/app/core/core.module.ts
@@ -7,8 +7,6 @@ import { SharedModule } from '../shared/shared.module';
 import { LoginComponent } from './components/login/login.component';
 import { BreadcrumbsComponent } from './components/breadcrumbs/breadcrumbs.component';
 
-
-
 @NgModule({
   declarations: [
     HeaderComponent,
